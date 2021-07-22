868b532d335027e6d4a7a246620bdaa2cdb3a68a
Temepest74 <raducata11@yahoo.com>
2021-07-22 11:07:27 +0300

    Update AdminCampaign.php

--- AdminCampaign.php
+++ AdminCampaign.php
@@ -323,7 +323,7 @@ class AdminCampaign extends ModuleAdminController
                             $content,
                             $this->module->validatePhone($phone),
                             $from
-                        ), ',', '"', "\0");
+                        ));
                     }
                     $result = $this->module->createBatch($fileUrl);
                     fclose($file);
